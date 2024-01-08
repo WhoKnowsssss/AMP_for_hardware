8b2f0dcfa390d3c936f0875b31229ea9041141d2
-T.K.- <t_k_233@outlook.com>
2024-01-07 18:30:09 -0800

    ADD: add ROS IPs

--- env.sh
+++ env.sh
@@ -1,2 +1,5 @@
 export LD_LIBRARY_PATH=/home/tk/Documents/mambaforge/envs/amp_hw/lib/
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+export ROS_MASTER_URI=http://10.0.255.63:11311
+export ROS_IP=10.0.255.59
