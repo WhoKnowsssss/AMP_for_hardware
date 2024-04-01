8dd01896477e626c1c213db0bd8c81ff2a3cb22a
-T.K.- <t_k_233@outlook.com>
2024-03-31 23:59:26 -0700

    ADD: separate bump from default

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -32,7 +32,7 @@ from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from legged_gym.envs.a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .base.legged_robot import LeggedRobot
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
-from .a1.a1_amp_config import A1AMPCfg, A1AMPCfgPPO
+from .a1.a1_amp_config import A1AMPCfg, A1AMPCfgPPO, A1AMPCfgPPOBump
 from .real.real_robot import RealMiniCheetah
 
 
@@ -42,6 +42,7 @@ from legged_gym.utils.task_registry import task_registry
 
 task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "a1_amp", LeggedRobot, A1AMPCfg(), A1AMPCfgPPO() )
+task_registry.register( "a1_amp_bump", LeggedRobot, A1AMPCfg(), A1AMPCfgPPOBump() )
 
 task_registry.register( "real_amp", RealMiniCheetah, A1AMPCfg(), A1AMPCfgPPO() )
 
